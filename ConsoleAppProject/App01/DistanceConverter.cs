9d7cda84d8465a238825ed801ec6cabb209bffa7
Alih99 <100180948@student.burnley.ac.uk>
2022-03-01 23:28:21 +0000

    code updated

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,37 @@
-﻿namespace ConsoleAppProject.App01
+﻿using System;
+
+namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This App will prompt the user to input a distance
+    /// measured in one unit and it will calculate and 
+    /// output the equicalent distance in another unit.
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Ali Hassan version 0.1
+    /// modified by 14/02/2022
     /// </author>
     public class DistanceConverter
     {
+        // constants for distance coversion
+        public const int FEET_IN_MILES = 5280;
+        public const double METRES_IN_MILES = 1609.34;
+        public const double FEET_IN_METRES = 3.28084;
+
+         //initialised instance variables
+        public double FromDistance { get; set; }
+        public double ToDistance { get; set; }
+        public DistanceUnits FromUnit { get; set; }
+        public DistanceUnits ToUnit { get; set; }
+
+        /// <summary>
+        /// Constructor of the DistanceConverter Class
+        /// </summary>
+        public DistanceConverter()
+        {
+            FromUnit = DistanceUnits.Miles;
+            ToUnit = DistanceUnits.Feet;
+        }
 
     }
 }
