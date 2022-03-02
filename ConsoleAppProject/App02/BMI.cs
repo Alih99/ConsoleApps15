7c0fe4f0303b2603d65e0cb6082f2413d528df9d
Alih99 <100180948@student.burnley.ac.uk>
2022-03-02 03:11:32 +0000

    additional code added

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -1,12 +1,27 @@
-﻿namespace ConsoleAppProject.App02
+﻿using System;
+using System.Text;
+
+namespace ConsoleAppProject.App02
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This app will use the body weight and height to calculate BMI Index
+    /// then classify the health risk.
+    /// User can input Imperial or Metric Unit.
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Ali Hassan version 0.3
     /// </author>
     public class BMI
     {
-    }
-}
+        //Bmi Index result
+        public double BmiIndex { get; set; }
+
+        //Weight and Height Units
+        public int Stone { get; set; }
+        public int Pound { get; set; }
+        public int Feet { get; set; }
+        public int Inch { get; set; }
+        public double Centimetres { get; set; }
+        public double Kilograms { get; set; }
+
+        public double Metres;
